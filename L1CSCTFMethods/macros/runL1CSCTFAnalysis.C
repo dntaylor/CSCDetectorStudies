d0047382a3024f81f6daedd5b18f1da0b190ca6b
Devin Taylor <dntaylor@wisc.edu>
2013-08-17 12:48:49 -0500

    copying necessary files from L1TriggerDPG

--- L1CSCTFMethods/macros/runL1CSCTFAnalysis.C
+++ L1CSCTFMethods/macros/runL1CSCTFAnalysis.C
@@ -3,10 +3,10 @@ gROOT->ProcessLine(".x UserCode/L1TriggerDPG/macros/initL1Analysis.C");
 gROOT->ProcessLine(".L UserCode/L1TriggerDPG/macros/Style.C");
 gROOT->ProcessLine("setTDRStyle()");
 gROOT->ProcessLine("gROOT->ForceStyle()");
-gROOT->ProcessLine(".L L1CSCTFAnalysis.C+");
+gROOT->ProcessLine(".L CSCDetectorStudies/L1CSCTFMethods/macros/L1CSCTFAnalysis.C+");
 
-L1CSCAnalysis t;
-t.OpenWithList("listOfFiles.txt");
+L1CSCTFAnalysis t;
+t.OpenWithList("CSCDetectorStudies/L1CSCTFMethods/macros/listOfFiles.txt");
 
 // run over a 1000 events
 t.run(1000);
